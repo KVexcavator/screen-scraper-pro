b9e1f41fc797f74f3fdb6d185f1b2fc147eeff69
Excavator <kvronin@gmail.com>
2026-01-29 19:54:23 +0500

    throw garbage

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,4 @@
-use game_screen_scraper::linux::display;
 use game_screen_scraper::linux::pipewire::get_wayland_portal;
-use slint::VecModel;
-use std::rc::Rc;
 
 slint::include_modules!();
 
@@ -12,30 +9,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // catch screens
     let session_type = std::env::var("XDG_SESSION_TYPE").unwrap_or_else(|_| "unknown".into());
-
-    let session_label = match session_type.as_str() {
-        "wayland" => "Wayland",
-        "x11" => "X11",
-        _ => "Unknown",
-    };
-
-    // TODO если wayland - тогда пытаемся работаем с zbus
     if session_type == "wayland"
         && let Err(e) = get_wayland_portal().await
     {
         eprintln!("Portal error: {e}");
     }
 
-    // TODO eсли X11 - тогда работаем с ним
-
-    // catch displays
-    let displays = display::get_displays()?;
-    let displays = Rc::new(VecModel::from(displays));
-
     let app = AppWindow::new()?;
-    // only for info, it doesn't need with ashpd crate
-    app.set_displays(displays.into());
-    app.set_session_type(session_label.into());
 
     app.run()?;
     Ok(())
