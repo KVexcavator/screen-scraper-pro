a9ee06b1f2434286b53068caa30c789ea527e38e
KVexcavator <kvronin@gmail.com>
2026-02-24 17:21:29 +0500

    can catch list of windows in terminal

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,22 +1,11 @@
 
-mod utils_linux;
-mod utils_windows;
-
 fn main(){
     #[cfg(target_os = "linux")]
         {
-            use crate::utils_linux::hello;
-
-            hello();
-
             let _ = linux_backend::run();
         }
     #[cfg(target_os = "windows")]
         {
-            use crate::utils_windows::hello;
-
-            hello();
-
             let _ = windows_backend::run();
         }
 }
