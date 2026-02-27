15ea843563b0a90480b3fa937871efb156db04dc
KVexcavator <kvronin@gmail.com>
2026-02-27 23:08:57 +0500

    new stop start architecture

--- app/src/win_ui.rs
+++ app/src/win_ui.rs
@@ -7,32 +7,17 @@ use windows_backend::capture_engine::CaptureEngine;
 use windows_backend::catcher::{WindowInfo, get_windows};
 use windows::Win32::Foundation::HWND;
 
+enum CaptureCommand {
+    Start(isize),
+    Stop,
+    Exit,
+}
+
 pub fn run() -> Result<(), Box<dyn std::error::Error>> {
     let ui = UiHandle::new()?;
 
-    // канал кадров из capture
-    let (sender, receiver) = mpsc::channel::<(u32, u32, Vec<u8>)>();
-
-    let ui_weak = ui.app.as_weak();
-
-    // Поток для UI отображения кадров
-    thread::spawn(move || {
-        while let Ok((w, h, data)) = receiver.recv() {
-            let ui_weak = ui_weak.clone();
-            slint::invoke_from_event_loop(move || {
-                if let Some(app) = ui_weak.upgrade() {
-                    let buffer = SharedPixelBuffer::<Rgba8Pixel>::clone_from_slice(&data, w, h);
-                    let image = Image::from_rgba8(buffer);
-                    app.set_preview(image);
-                }
-            })
-                .ok();
-        }
-    });
-
-    let windows_cache = Arc::new(Mutex::new(Vec::<WindowInfo>::new()));
-
     // Обновление списка окон
+    let windows_cache = Arc::new(Mutex::new(Vec::<WindowInfo>::new()));
     {
         let cache = windows_cache.clone();
         let ui_ref = ui.app.as_weak();
@@ -49,24 +34,93 @@ pub fn run() -> Result<(), Box<dyn std::error::Error>> {
         });
     }
 
+    // Поток для UI отображения кадров
+    let ui_weak = ui.app.as_weak();
+    let (frame_tx, frame_rx) = mpsc::channel::<(u32, u32, Vec<u8>)>();
+    thread::spawn(move || {
+        while let Ok((w, h, data)) = frame_rx.recv() {
+            let ui_weak = ui_weak.clone();
+            slint::invoke_from_event_loop(move || {
+                if let Some(app) = ui_weak.upgrade() {
+                    let buffer =
+                        SharedPixelBuffer::<Rgba8Pixel>::clone_from_slice(&data, w, h);
+                    let image = Image::from_rgba8(buffer);
+                    app.set_preview(image);
+                }
+            }).ok();
+        }
+    });
+
+    // Worker поток для команд(CaptureCommand)
+    let (cmd_tx, cmd_rx) = mpsc::channel::<CaptureCommand>();
+    thread::spawn(move || {
+        use std::sync::atomic::{AtomicBool, Ordering};
+        use std::sync::Arc;
+
+        let mut engine: Option<CaptureEngine> = None;
+        let mut running_flag: Option<Arc<AtomicBool>> = None;
+
+        while let Ok(cmd) = cmd_rx.recv() {
+            match cmd {
+                CaptureCommand::Start(hwnd_value) => {
+                    if let Some(flag) = &running_flag {
+                        flag.store(false, Ordering::SeqCst);
+                    }
+
+                    let hwnd = HWND(hwnd_value as _);
+
+                    let running = Arc::new(AtomicBool::new(true));
+                    running_flag = Some(running.clone());
+
+                    let frame_tx_clone = frame_tx.clone();
+
+                    if engine.is_none() {
+                        engine = Some(CaptureEngine::init().unwrap());
+                    }
+
+                    if let Some(engine_ref) = engine.as_mut() {
+                        engine_ref
+                            .start_with_flag(hwnd, running, move |w, h, data| {
+                                frame_tx_clone.send((w, h, data)).ok();
+                            })
+                            .ok();
+                    }
+                }
+
+                CaptureCommand::Stop => {
+                    if let Some(flag) = &running_flag {
+                        flag.store(false, Ordering::SeqCst);
+                    }
+                    running_flag = None;
+                }
+
+                CaptureCommand::Exit => break,
+            }
+        }
+    });
+
     // Обработчик выбора окна
+    // фрейм стартует при выборе
     {
         let cache = windows_cache.clone();
+        let tx = cmd_tx.clone();
 
         ui.app.on_window_selected(move |index| {
             let windows = cache.lock().unwrap();
             if let Some(selected) = windows.get(index as usize) {
                 let hwnd_value = selected.hwnd.0 as isize;
-
-                // Запускаем CaptureEngine в отдельном MTA-потоке
-                let sender_clone = sender.clone();
-                thread::spawn(move || {
-                    let hwnd = HWND(hwnd_value as _);
-                    let engine = CaptureEngine::init().unwrap();
-
-                    engine.start(hwnd, move|| { eprintln!("✓ Capture START");});
-                });
+                tx.send(CaptureCommand::Start(hwnd_value)).ok();
             }
+        });
+    }
+
+    // Обработчик остановки фрейма
+    {
+        let tx = cmd_tx.clone();
+
+        ui.app.on_stop_capture(move || {
+            tx.send(CaptureCommand::Stop).ok();
+            eprintln!("Button click STOP capture ==================>>>>>>>>>>>>");
         });
     }
 
--- windows-backend/src/capture_engine.rs
+++ windows-backend/src/capture_engine.rs
@@ -19,15 +19,22 @@ use windows::{
     },
     core::*,
 };
+use std::sync::{
+    atomic::{AtomicBool, Ordering},
+    Arc,
+};
 
 pub struct CaptureEngine {
     d3d_device: IDirect3DDevice,
+    running: Arc<AtomicBool>,
 }
 
 impl CaptureEngine {
     pub fn init() -> Result<Self> {
         // Шаг 1 — Инициализация COM
-        unsafe { RoInitialize(RO_INIT_MULTITHREADED)?; }
+        // unsafe { RoInitialize(RO_INIT_MULTITHREADED)?; }
+        // может работать лучше
+        unsafe { RoInitialize(RO_INIT_SINGLETHREADED)?; }
 
         // Шаг 2 — Создание D3D11 устройства
         let mut device: Option<ID3D11Device> = None;
@@ -51,13 +58,27 @@ impl CaptureEngine {
         let inspectable: IInspectable = unsafe { CreateDirect3D11DeviceFromDXGIDevice(&dxgi_device)? };
         let d3d_device: IDirect3DDevice = inspectable.cast()?;
 
-        Ok(Self { d3d_device })
+        Ok(Self {
+            d3d_device,
+            running: Arc::new(AtomicBool::new(false)),
+        })
     }
 
-    pub fn start<F>(&self, hwnd: HWND, mut on_frame: F) -> Result<()>
-    where F: FnMut() + Send + 'static,
+    pub fn stop(&self) -> Result<()> {
+        self.running.store(false, Ordering::SeqCst);
+        eprintln!("Stopping capture engine ==================>>>>>>>>>>>>");
+        Ok(())
+    }
+
+    pub fn start_with_flag<F>(
+        &mut self,
+        hwnd: HWND,
+        running: Arc<AtomicBool>,
+        mut on_frame: F,
+    ) -> Result<()>
+    where
+        F: FnMut(u32, u32, Vec<u8>) + Send + 'static,
     {
-        // Шаг 4 — Создать FramePool
         let item = create_capture_item(hwnd)?;
         let size = item.Size()?;
 
@@ -68,57 +89,120 @@ impl CaptureEngine {
             size,
         )?;
 
-        // Шаг 5 — Сессия
         let session = frame_pool.CreateCaptureSession(&item)?;
         session.StartCapture()?;
 
-        // Шаг 6 — Получение кадров
         let _token = frame_pool.FrameArrived(
-            &TypedEventHandler::<Direct3D11CaptureFramePool, IInspectable>::new({
-                move |pool_opt: &Option<Direct3D11CaptureFramePool>, _| {
-                    if let Some(pool) = pool_opt {
-                        match pool.TryGetNextFrame() {
-                            Ok(frame) => {
-                                // TODO create videofile
-                                // понять что еще есть вo frame
-                                // например frame.Surface()
-                                // как получать ID3D11Texture2D
-                                // дальше либо:
-                                //  - копировать в staging texture
-                                //  - отдавать в encoder
-                                match frame.ContentSize() {
-                                    Ok(size) => {
-                                        println!("FRAME: {}x{}", size.Width, size.Height);
-                                        on_frame();
-                                    }
-                                    Err(e) => println!("ContentSize error: {:?}", e),
+            &TypedEventHandler::<Direct3D11CaptureFramePool, IInspectable>::new(
+                move |pool: &Option<Direct3D11CaptureFramePool>, _| {
+                    if let Some(pool) = pool {
+                        if let Ok(frame) = pool.TryGetNextFrame() {
+                            if let Ok(size) = frame.ContentSize() {
+                                if let (Ok(w), Ok(h)) = (
+                                    u32::try_from(size.Width),
+                                    u32::try_from(size.Height),
+                                ) {
+                                    println!("FRAME: {}x{}", w, h);
+                                    let fake = vec![255u8; (w * h * 4) as usize];
+                                    on_frame(w, h, fake);
+                                    // on_frame(w, h, vec![]);
                                 }
                             }
-                            Err(e) => println!("No frame: {:?}", e),
                         }
                     }
                     Ok(())
-                }
-            })
+                },
+            ),
         )?;
 
-        // Message pump для MTA WinRT
-        loop {
+        while running.load(Ordering::SeqCst) {
             unsafe {
                 let mut msg = std::mem::MaybeUninit::<MSG>::uninit();
                 let pmsg = msg.as_mut_ptr();
 
-                if GetMessageW(pmsg, HWND(std::ptr::null_mut()), 0, 0).as_bool() {
-                    let msg_filled = msg.assume_init();
+                if PeekMessageW(pmsg, HWND(std::ptr::null_mut()), 0, 0, PM_REMOVE).as_bool() {
+                    let msg = msg.assume_init();
                     #[allow(unused)]
-                    TranslateMessage(&msg_filled as *const _);
-                    DispatchMessageW(&msg_filled as *const _);
+                    TranslateMessage(&msg);
+                    DispatchMessageW(&msg);
                 }
             }
-            std::thread::sleep(std::time::Duration::from_millis(10));
+
+            std::thread::sleep(std::time::Duration::from_millis(5));
         }
 
+        session.Close()?;
+        frame_pool.Close()?;
+
+        Ok(())
     }
+
+    // pub fn start<F>(&self, hwnd: HWND, mut on_frame: F) -> Result<()>
+    // where F: FnMut() + Send + 'static,
+    // {
+    //     self.stop()?;
+    //     // Шаг 4 — Создать FramePool
+    //     let item = create_capture_item(hwnd)?;
+    //     let size = item.Size()?;
+    //
+    //     let frame_pool = Direct3D11CaptureFramePool::Create(
+    //         &self.d3d_device,
+    //         DirectXPixelFormat::B8G8R8A8UIntNormalized,
+    //         2,
+    //         size,
+    //     )?;
+    //
+    //     // Шаг 5 — Сессия
+    //     let session = frame_pool.CreateCaptureSession(&item)?;
+    //     session.StartCapture()?;
+    //
+    //     // Шаг 6 — Получение кадров
+    //     let _token = frame_pool.FrameArrived(
+    //         &TypedEventHandler::<Direct3D11CaptureFramePool, IInspectable>::new({
+    //             move |pool_opt: &Option<Direct3D11CaptureFramePool>, _| {
+    //                 if let Some(pool) = pool_opt {
+    //                     match pool.TryGetNextFrame() {
+    //                         Ok(frame) => {
+    //                             // TODO create videofile
+    //                             // понять что еще есть вo frame
+    //                             // например frame.Surface()
+    //                             // как получать ID3D11Texture2D
+    //                             // дальше либо:
+    //                             //  - копировать в staging texture
+    //                             //  - отдавать в encoder
+    //                             match frame.ContentSize() {
+    //                                 Ok(size) => {
+    //                                     println!("FRAME: {}x{}", size.Width, size.Height);
+    //                                     on_frame();
+    //                                 }
+    //                                 Err(e) => println!("ContentSize error: {:?}", e),
+    //                             }
+    //                         }
+    //                         Err(e) => println!("No frame: {:?}", e),
+    //                     }
+    //                 }
+    //                 Ok(())
+    //             }
+    //         })
+    //     )?;
+    //
+    //     // Message pump для MTA WinRT
+    //     loop {
+    //         unsafe {
+    //             let mut msg = std::mem::MaybeUninit::<MSG>::uninit();
+    //             let pmsg = msg.as_mut_ptr();
+    //
+    //             if GetMessageW(pmsg, HWND(std::ptr::null_mut()), 0, 0).as_bool() {
+    //                 let msg_filled = msg.assume_init();
+    //                 #[allow(unused)]
+    //                 TranslateMessage(&msg_filled as *const _);
+    //                 DispatchMessageW(&msg_filled as *const _);
+    //             }
+    //         }
+    //         std::thread::sleep(std::time::Duration::from_millis(10));
+    //     }
+    //
+    // }
 }
 
 // Шаг 3 — Получить GraphicsCaptureItem из HWND
